9fabb9ca8825e1fe8ffcac9a59ed131bc8bbde38
Raviteja <jazzycart16@gmail.com>
2019-08-17 23:08:23 +0530

    testing

---
+++ src/main/java/com/sb/gitcheck/githooks/GithooksApplication.java
@@ -0,0 +1,13 @@
+package com.sb.gitcheck.githooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithooksApplication.class, args);
+	}
+
+}
